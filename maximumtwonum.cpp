f5750d6c84116b32497fa027cabccdaf0518e7ef
Rajputanshul <96253988+Rajputanshul@users.noreply.github.com>
2021-12-17 14:01:45 +0530

    maximum two number

---
+++ maximumtwonum.cpp
@@ -0,0 +1,17 @@
+// find maximum between two numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cin>>num1>>num2;
+	if(num1>num2)
+	{
+		cout<<num1<<" Is greater than "<<num2<<endl;
+	}
+	else
+	{
+			cout<<num2<<" Is greater than "<<num1<<endl;
+	}
+}
