035a5590138889cf32e86e39dc7f1ed4bedd3e6d
Rajputanshul <96253988+Rajputanshul@users.noreply.github.com>
2021-12-19 22:39:15 +0530

    19=12=21

---
+++ pattern 8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=65;
+	for(int i=1;i<=4;i++)
+	{
+	    for(int j=1;j<=i;j++)
+		{
+			cout<<char(n)<<" ";
+			n=n+1;
+	    }
+		cout<<endl;
+	}
+	
+}
